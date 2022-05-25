2a14aaa7ef06704ea82ad1b8236ac5fa3439ea93
Kishor-Kant-Tiwari <kishor.tiwari_cs20@gla.ac.in>
2022-05-25 20:04:09 +0530

    few changes

--- Store.php
+++ Store.php
@@ -42,8 +42,13 @@
     
     <section id="home" >
         <h1 class="h-primary">Welcome to MyOnlineMeal</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et harum cupiditate odit labore vitae.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum enim magnam delectus excepturi reiciendis.</p>
+        <p> Our Kitchen strives to source local, sustainable and organic when possible. We work hard to source premium
+            ingredients and we cook everything from scratch with love. We also do our best to pay our employees living
+            wages (tips are shared with all employees, including kitchen staff) and to reduce our environmental
+            footprint wherever we can. Overall, these factors translate to higher menu prices, but we hope that you find
+            value and feel a sense of comfort in knowing that we aim to get better everyday at doing what is important
+            to us.
+        </p>
         
     </section>
     <?php
